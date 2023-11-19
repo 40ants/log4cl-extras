b1a12eb3feeec7e7709b7ff809b2234b893feb3b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-11-19 14:20:33 +0000

    A few changes.

---
+++ src/core.lisp
@@ -0,0 +1,6 @@
+(uiop:define-package #:log4cl-extras
+  (:use #:cl)
+  (:nicknames #:log4cl-extras/core)
+  (:import-from #:log4cl-extras/config)
+  (:import-from #:log4cl-extras/error))
+(in-package #:log4cl-extras)
